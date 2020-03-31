3fbdd0c44810e61fc89d0f45747467eeb628730f
Adam Welnicki <adam.welnicki@gmail.com>
2020-03-31 14:17:24 +0200

    003 General routing added

---
+++ src/components/Home/HomeHeader/HomeHeader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HomeHeader = () => {
+    return (
+        <span>zaloguj</span>
+    )
+};
+
+export default HomeHeader;
---
+++ src/components/Home/HomeThreeColumns/HomeThreeColumns.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HomeThreeColumns = () => {
+    return (
+        <span>wyloguj</span>
+    )
+};
+
+export default HomeThreeColumns;
