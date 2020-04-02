d80ef3ace6bd09cce1d64efa6efe60613ddae206
Adam Welnicki <adam.welnicki@gmail.com>
2020-04-02 18:30:36 +0200

    010 Section Foundations basic logic applied

--- src/components/Home/Foundations/Foundations.js
+++ src/components/Home/Foundations/Foundations.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
+import FoundationType from "./FoundationType/FoundationType";
 
-const Foundations = ({ id }) => {
+const Foundations = ({id}) => {
+    const [type, setType] = useState(1);
+
+    const buttonClickHandler = (e) => {
+        setType(+e.target.value);
+    };
+
     return (
-        <h1 id={id} style={{ height: '100vh'  }}>Komponent Foundations</h1>
+        <section id={id}>
+            <div className={"title_container"}>
+                <h3>Komu pomagamy?</h3>
+            </div>
+            <div className={"btn_container"}>
+                <button onClick={buttonClickHandler}
+                        className={type === 1 ? "active" : ""}
+                        value={1}>Fundacjom</button>
+                <button onClick={buttonClickHandler}
+                        className={type === 2 ? "active" : ""}
+                        value={2}>Organizacjom<br/>pozarządowym</button>
+                <button onClick={buttonClickHandler}
+                        className={type === 3 ? "active" : ""}
+                        value={3}>Lokalnym<br/>zbiórkom</button>
+            </div>
+            {type === 1 && <FoundationType text={"FoundationType 1"}/>}
+            {type === 2 && <FoundationType text={"FoundationType 2"}/>}
+            {type === 3 && <FoundationType text={"FoundationType 3"}/>}
+        </section>
     )
 };
 
